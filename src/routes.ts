6f38bc8cf3d1a0e27a76ee5c4c6e06707c0ea5fd
leocalmeida <leonardocaetanoalmeida@gmail.com>
2020-06-02 22:42:23 -0300

    Dia2 NLW

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+//1:17:29 Dia 02
+const routes = express.Router();
+
+routes.get("/itmes", (req, res) => {});
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-//01/06/2020
 import express from "express";
+import routes from "./routes";
+import path from "path";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.json({ ok: "leonardo" });
-});
+app.use(express.json());
+app.use(routes);
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
